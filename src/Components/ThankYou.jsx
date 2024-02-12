66e1051e9418f797a515340026db0f232c2dd1c6
debugmanoj <manojkumarwork01@gmail.com>
2024-02-12 14:48:13 +0530

    Thank you

--- src/Components/ThankYou.jsx
+++ src/Components/ThankYou.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function ThankYou() {
   return <>
    <div className='text-center' style={{"marginTop":"10%"}} >
-    <div className='' style={{"fontWeight":"900","fontSize":"70px"}}>Thank you creating the account</div>
+    <div className='' style={{"fontWeight":"900","fontSize":"30px"}}>Thank you creating the account</div>
+    <h6>Check your mail for activation</h6>
+
     <small>Ready to shorten Url</small>
     <br />
     <br />
